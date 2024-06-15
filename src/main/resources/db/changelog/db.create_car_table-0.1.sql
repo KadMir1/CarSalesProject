8dc3128a8cd19002896a6ee2e15bff2b88958fde
KadMir1 <zlobnisom@gmail.com>
2024-06-15 12:25:43 +0300

    Add table and insert files in db

--- src/main/resources/db/changelog/db.create_car_table-0.1.sql
+++ src/main/resources/db/changelog/db.create_car_table-0.1.sql
@@ -5,7 +5,7 @@
 CREATE TABLE car (
     id serial PRIMARY KEY ,
     brand_id int NOT NULL REFERENCES brand(id),
-    model_id int NOT NULL REFERENCES model(id),
+    model_id int NOT NULL REFERENCES car_model(id),
     body_id int NOT NULL REFERENCES body(id),
     engine_id int NOT NULL REFERENCES engine(id),
     transmission_id int NOT NULL REFERENCES transmission(id),
